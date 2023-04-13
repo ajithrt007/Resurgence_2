dd51c7c70f192be3294c65767c0077fbb2008ead
ajithrt007 <ajithrt170@gmail.com>
2023-04-14 00:03:14 +0530

    Initial Commit5

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -9,9 +9,9 @@ export default function Sponsor(){
                 <img src="/sponsor.svg" alt="" className='w-[15vw]'/>
             </div>
             <div className='flex flex-col gap-2'>
-                <p className='points'>Msigma Gokulam is an ed-tech company focused on "Everything Engineering"</p>
-                <p className='points'>We provide classes for GATE, IES, PSU's with the best teachers in the country, including an IES first ranker holding the highest recorded marks in an IES exam. </p>
-                <p className='points'>To empower 'engineering aspirants' to become engineers we also provide coaching for Btech.</p>
+                <p className='points'>Msigma Gokulam is an ed-tech company focused on &quot;Everything Engineering&quot;</p>
+                <p className='points'>We provide classes for GATE, IES, PSU&apos;s with the best teachers in the country, including an IES first ranker holding the highest recorded marks in an IES exam. </p>
+                <p className='points'>To empower &apos;engineering aspirants&apos; to become engineers we also provide coaching for Btech.</p>
                 <p className='points'>Our R&D department works on top AI/ML, IoT and Embedded Systems projects for governmental and non governmental companies like Mahindra & Mahindra. (They also provide assistance for final year engineering projects.)</p>
                 <p className='points'>Our IT department helps businesses with next-generation digital services and consulting.</p>
             </div>
