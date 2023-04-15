bc053be5fd47258277522df748de57c6d26e250c
ajithrt007 <ajithrt170@gmail.com>
2023-04-15 13:42:51 +0530

    Analytics added, prev fix

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,11 @@ export default function About(){
         <div className='p-[4vw]' id='about'>
             <div className='py-[4vw] about flex flex-col gap-[10vh]'>
                 <div className='flex aboutContent'>
-                    <div className='flex flex-col gap-5 px-[4vw]'>
+                    <div className='flex flex-col gap-5 px-[4vw] w-[50%]'>
                         <h1>About Resurgence 2.0</h1>
                         <p>Resurgence was an online tech-fest, conducted by IEEE SCT SB to cater to all the first year SCTians. But now, after a year long hiatus, Resurgence 2.0 is here with a new look and sparks are about to fly. This year, Resurgence 2.0 is an open-for-all, batch-wise competitive extravaganza. Lead your batch to victory by showing your skills and prowess in the various events and be the first one to reach glory. With an event list that encompasses technical skills and creative enterprises, help your batch beat all others by being a part of Resurgence 2.0. Are you ready to win it all? Register now in any and all events of your choosing, and may the best batch be crowned champions. </p>
                     </div>
-                    <div className='flex flex-col gap-5 px-[4vw]'>
+                    <div className='flex flex-col gap-5 px-[4vw] w-[50%]'>
                         <h1>About IEEE SCT SB</h1>
                         <p>IEEE started off in Sree Chitra Thirunal College Of Engineering with the registration of members on 23rd October 2009 with 30 members in its first phase. The Student Branch is one of the top 10 SBs in the Travancore Hub. Sree Chitra Thirunal College Of Engineering is one of the leading engineering colleges in Thiruvananthapuram, Kerala</p>
                     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import HeroSection from "../components/HeroSection.js";
 import Largest from "../components/Largest.js";
 import Container from "../components/Container.js";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -19,11 +20,26 @@ export default function Home() {
           property="og:description"
           content="IEEE SCT SB is here with yet another version of Resurgence. RESURGENCE 2.0 is a batch-wise competition conducted exclusively for SCTians with many activities like hackathons, competitions, etc."
         />
-        <meta
-          property="og:image"
-          content="/resurgence.svg"
-        />
+        <meta property="og:image" content="/resurgence.svg" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-T5CSNYS9YF"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-T5CSNYS9YF', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
       <Navbar />
       <HeroSection />
       <Largest />
