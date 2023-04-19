84353a04f2979d463ac0f96e9574b1e652d15917
ajithrt007 <ajithrt170@gmail.com>
2023-04-20 02:35:22 +0530

    Winner details and score update

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,68 +1,82 @@
-import React from 'react'
+import React from "react";
 
-export default function Scoreboard(){
-    const scoreboard = [
-        ['R2A' , 57],
-        ['R2B' , 15],
-        ['R4A' , 34],
-        ['R4B' , 24],
-        ['R6A' , 0],
-        ['R6B' , 0],
-        ['R8' , 0],
-        ['T2A' , 245],
-        ['T2B' , 39],
-        ['T4A' , 17],
-        ['T4B' , 15],
-        ['T6A' , 0],
-        ['T6B' , 0],
-        ['T8A' , 0],
-        ['T8B' , 0],
-        ['M2A' , 112],
-        ['M2B' , 92],
-        ['M4A' , 15],
-        ['M4B' , 77],
-        ['M6A' , 20],
-        ['M6B' , 5],
-        ['M8' , 0],
-        ['B2' , 90],
-        ['B4' , 5],
-        ['B6' , 10],
-        ['B8' , 0],
-        ['P6' , 5],
-        ['P8' , 0],
-        ['U2' , 10],
-        ['U4' , 20],
-        ['U6' , 0],
-        ['U8' , 0]
-    ];
-    for (var x = 0;x<scoreboard.length;x++){
-        for (var y = 0;y<(scoreboard.length-1);y++){
-            if(scoreboard[y][1]<scoreboard[y+1][1]){
-                let temp = scoreboard[y+1];
-                scoreboard[y+1] = scoreboard[y];
-                scoreboard[y] = temp;
-            }
-        }
+export default function Scoreboard() {
+  const scoreboard = [
+    ["R2A", 388],
+    ["R2B", 25],
+    ["R4A", 196],
+    ["R4B", 106],
+    ["R6A", 6],
+    ["R6B", 115],
+    ["R8", 0],
+    ["T2A", 982],
+    ["T2B", 121],
+    ["T4A", 82],
+    ["T4B", 28],
+    ["T6A", 21],
+    ["T6B", 9],
+    ["T8A", 0],
+    ["T8B", 5],
+    ["M2A", 302],
+    ["M2B", 203],
+    ["M4A", 118],
+    ["M4B", 140],
+    ["M6A", 70],
+    ["M6B", 6],
+    ["M8", 0],
+    ["B2", 169],
+    ["B4", 15],
+    ["B6", 20],
+    ["B8", 7],
+    ["P6", 5],
+    ["P8", 0],
+    ["U2", 10],
+    ["U4", 35],
+    ["U6", 30],
+    ["U8", 0],
+  ];
+  for (var x = 0; x < scoreboard.length; x++) {
+    for (var y = 0; y < scoreboard.length - 1; y++) {
+      if (scoreboard[y][1] < scoreboard[y + 1][1]) {
+        let temp = scoreboard[y + 1];
+        scoreboard[y + 1] = scoreboard[y];
+        scoreboard[y] = temp;
+      }
     }
-    var count = 1;
-    console.log(scoreboard)
-    return(
-        <div className='flex flex-col gap-10 p-[4vw]' id='score'>
-            <h1 className='heading'>Scoreboard</h1>
-            <div className='flex flex-col items-center gap-5 classes'>
-                {
-                    scoreboard.slice(0, 10).map((element,count) => {
-                        count = count + 1
-                        return (
-                            <div className='flex justify-between w-[40vw] p-[20px] bg-black rounded-xl' key={element[0]}>
-                                <p>{count}</p>
-                                <p>{element[0]}</p>
-                                <p>{element[1]}</p>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+  }
+  var count = 1;
+  console.log(scoreboard);
+  return (
+    <>
+      <div className="flex flex-col gap-10 p-[4vw]" id="score">
+        <h1 className="heading">Scoreboard</h1>
+        <div className="flex flex-col items-center gap-5 classes">
+          {scoreboard.slice(0, 10).map((element, count) => {
+            count = count + 1;
+            return (
+              <div
+                className="flex justify-between w-[40vw] p-[20px] bg-black rounded-xl"
+                key={element[0]}
+              >
+                <p>{count}</p>
+                <p>{element[0]}</p>
+                <p>{element[1]}</p>
+              </div>
+            );
+          })}
         </div>
-    )
+      </div>
+      <div className="flex flex-col gap-10 p-[4vw]" id="score">
+        <h1 className="heading text-center">Resurgence 2.0 Winner</h1>
+        <div className="flex flex-col items-center gap-5 classes">
+        <div
+                className="flex justify-between w-[40vw] p-[20px] px-[40px] bg-black rounded-xl winner"
+              >
+                <p>T2A</p>
+                <p>982</p>
+              </div>
+        </div>
+      </div>
+    </>
+  )
 }
